cf862912183405282af181371a35556b5314ac2e
Sam Deane <deane_sam@yahoo.com>
2022-01-20 18:11:40 -0500

    More initial push
    Also including a few new changes and effects

---
+++ Project-Narrative/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    private bool inNPC;
+    [SerializeField]
+    private GameObject interactUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NPCEnterExit(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NPCEnterExit(other);
+    }
+
+    private void NPCEnterExit(Collider other)
+    {
+        NPC potentialNPC;
+        if (other.TryGetComponent<NPC>(out potentialNPC))
+        {
+            inNPC = !inNPC;
+            interactUI.SetActive(inNPC);
+            if (inNPC)
+            {
+                interactUI.transform.GetChild(0).GetComponent<Text>().text = "Talk to " + potentialNPC.GetName();
+            }
+
+        }
+    }
+}
